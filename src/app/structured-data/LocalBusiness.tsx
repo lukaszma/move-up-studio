d9b0c92bbff5b13c98e4f199db9f4b299df373f5
Łukasz MAtyja <lukasz.a.matyja@gmail.com>
2023-01-30 22:06:20 +0100

    redirection added

--- src/app/structured-data/LocalBusiness.tsx
+++ src/app/structured-data/LocalBusiness.tsx
@@ -1,65 +1,62 @@
-export const LocalBusiness = {
+export const LocalBusiness = () => ({
+  __html: `{
     "@context": "https://schema.org",
-    "@type": "Restaurant",
-    "image": [
-        "/android-chrome-192x192.png",
-        "/android-chrome-512x512.png",
-        "/favicon-16x16.png",
-        "/favicon-32x32.png",
+    "@type": "Aerial sports center",
+    image: [
+      "/android-chrome-192x192.png",
+      "/android-chrome-512x512.png",
+      "/favicon-16x16.png",
+      "/favicon-32x32.png",
     ],
-    "name": "Move Up Studio",
-    "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "1 Maja 21",
-        "addressLocality": "Częstochowa",
-        "postalCode": "42-217",
-        "addressCountry": "PL"
+    name: "Move Up Studio",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "1 Maja 21",
+      addressLocality: "Częstochowa",
+      postalCode: "42-217",
+      addressCountry: "PL",
     },
-    "review": {
-        "@type": "Review",
-        "reviewRating": {
-            "@type": "Rating",
-            "ratingValue": "5",
-            "bestRating": "5"
-        },
+    review: {
+      "@type": "Review",
+      reviewRating: {
+        "@type": "Rating",
+        ratingValue: "5",
+        bestRating: "5",
+      },
     },
-    "geo": {
-        "@type": "GeoCoordinates",
-        "latitude": 50.79863113105372,
-        "longitude": 19.116222930685222
+    geo: {
+      "@type": "GeoCoordinates",
+      latitude: 50.79863113105372,
+      longitude: 19.116222930685222,
     },
-    "url": "https://moveupstudio.netlify.app",
-    "telephone": "721 829 352",
-    "openingHoursSpecification": [
-        {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": [
-                "Monday",
-                "Friday"
-            ],
-            "opens": "16:00",
-            "closes": "20:00"
-        },
-        {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": [
-                "Tuesday",
-            ],
-            "opens": "16:00",
-            "closes": "20:30"
-        },
-        {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": ["Wednesday", "Thursday"],
-            "opens": "16:00",
-            "closes": "21:00"
-        },
-        {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": "Saturday",
-            "opens": "10:00",
-            "closes": "13:00"
-        }
+    url: "https://moveupstudio.netlify.app",
+    telephone: "721 829 352",
+    openingHoursSpecification: [
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: ["Monday", "Friday"],
+        opens: "16:00",
+        closes: "20:00",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: ["Tuesday"],
+        opens: "16:00",
+        closes: "20:30",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: ["Wednesday", "Thursday"],
+        opens: "16:00",
+        closes: "21:00",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: "Saturday",
+        opens: "10:00",
+        closes: "13:00",
+      },
     ],
-    "acceptsReservations": "True"
-}
+    acceptsReservations: "True",
+  }`
+});
